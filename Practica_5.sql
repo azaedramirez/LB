9760bbc20c5595fa1cf27590d4d89cc2257778ba
azaedramirez <42654646+azaedramirez@users.noreply.github.com>
2019-05-02 02:17:02 -0500

    Practica 5

---
+++ Practica_5.sql
@@ -0,0 +1,228 @@
+
+insert into Plato values(1001,'Pizza',80,'Pizza Grande','1');
+insert into Plato values(1002,'Pizza',60,'Pizza Mediana','1');
+insert into Plato values(1003,'Pizza',50,'Pizza Individual','1');
+insert into Plato values(1004,'Pasta',60,'Pasta Individual','1');
+insert into Plato values(1005,'Pasta',80,'Pasta Grande','3');
+insert into Plato values(1006,'Hamburguesa',70,'Hamburguesa Clasica','5');
+insert into Plato values(1007,'Hamburguesa',90,'Hamburguesa Doble','1');
+insert into Plato values(1008,'Hamburguesa',70,'Hamburguesa con Tocino','1');
+insert into Plato values(1009,'Hamburguesa',70,'Hamburguesa con Champiñones','1');
+insert into Plato values(1010,'Dedos de Queso',70,'Dedos de Queso','2');
+insert into Plato values(1011,'Papas a la Francesa',40,'Papas sin Queso','3');
+insert into Plato values(1012,'Papas con Queso',55,'Papas con Queso','4');
+insert into Plato values(1013,'Extras',88,'Extra de cocina','1');
+
+
+insert into Bebida values(1001,'Tequila',60.99,'Tequila Jose Cuervo Especial','100');
+insert into Bebida values(1002,'Whisky',60.99,'Whisky Johnnie Walker, Red Label','50');
+insert into Bebida values(1003,'Ron',60.99,'Ron Bacardi, Carta Blanca','40');
+insert into Bebida values(1004,'Brandy',60.99,'Brandy Don Pedro','23');
+insert into Bebida values(1005,'Vodka',60.99,'Vodka Absolut','14');
+insert into Bebida values(1006,'Cerveza',40.99,'Nacional','47');
+insert into Bebida values(1007,'Cerveza',50.99,'Importada','100');
+insert into Bebida values(1008,'Perla Negra',70.99,'Jagermeister, Whisky, Bebida Energizante','80');
+insert into Bebida values(1009,'Paloma',50.99,'Tequila(blanco), Limon, Sal, Refresco de toronja','50');
+insert into Bebida values(1010,'Vampiro',55.99,'Tequila, Sangrita, Sal, Limon, Refresco de toronja','30');
+insert into Bebida values(1011,'Cuba',50.99,'Refresco(Normal), Ron','101');
+insert into Bebida values(1012,'Vodka con Jugo',50.99,'Vodka, Jugo(Piña, Arandano, Naranja','11');
+insert into Bebida values(1013,'Shots',100.99,'Ronda de 4 Caballitos (Cualquier Licor)','10');
+insert into Bebida values(1014,'Agua',20.99,'Agua natural','95');
+insert into Bebida values(1015,'Agua de Sabor',35.99,'Agua de Sabor(Limon, Jamaica, Piña)','81');
+insert into Bebida values(1016,'Refresco',30.99,'Cualquier tipo de Refresco','100');
+
+
+insert into Empleado values(1001,'Alfonso','Garza','Garcia','1990-09-11',81125678,'Flamingo 947',1001);
+insert into Empleado values(1002,'Pedro','Lopez','Garza','1995-04-21',86945678,'Alcatraz 492',1003);
+insert into Empleado values(1003,'Francisco','Leal','Tovar','1990-11-11',80915678,'Trebol 333',1001);
+insert into Empleado values(1004,'Chad','Perez','Leija','1993-09-11',83495678,'Kiev 492',1002);
+insert into Empleado values(1005,'Juan','Rodriguez','Contreras','1911-11-11',86483678,'Cipres 969',1001);
+insert into Empleado values(1006,'Alberto','Farias','Castro','1994-11-22',86946948,'Lino 448',1003);
+insert into Empleado values(1007,'Hugo','Rodriguez','Gomez','1988-08-19',86942580,'Lardo 694',1001);
+insert into Empleado values(1008,'Hector','Gonzales','Martinez','1881-11-19',86969488,'Monclova 490',1002);
+insert into Empleado values(1009,'Alex','Diaz','Moreno','1993-01-13',86941036,'Parras 968',1001);
+insert into Empleado values(1010,'Andres','Ruiz','Morales','1996-04-13',86159478,'Sotelo 598',1003);
+insert into Empleado values(1011,'Martin','Prieto','Vidal','1994-11-18',86909638,'Violeta 448',1002);
+insert into Empleado values(1012,'Osvaldo','Garza','Lopez','1991-10-20',86898980,'Tabaco 694',1001);
+insert into Empleado values(1013,'Ivan','Blanco','Rubio','1997-09-01',81588488,'Flor 490',1002);
+insert into Empleado values(1014,'Yahir','Luna','Campos','1992-06-28',89991036,'Ceibo 968',1001);
+insert into Empleado values(1015,'Moises','Mier','Torres','1999-12-25',86106362,'Noche Buena 598',1003);
+
+
+
+
+insert into ExEmpleado values(001,'Maria','Garza','Garcia');
+insert into ExEmpleado values(002,'Valeria','Lopez','Garza');
+insert into ExEmpleado values(003,'Francisca','Leal','Tovar');
+insert into ExEmpleado values(004,'Chadir','Perez','Wheeler');
+insert into ExEmpleado values(005,'Zed','Rodriguez','Contreras');
+insert into ExEmpleado values(006,'Yasuo','Farias','Joe');
+insert into ExEmpleado values(007,'Angel','Rrevilla','Gomez');
+insert into ExEmpleado values(008,'Hector','Gonzales','Juarez');
+insert into ExEmpleado values(009,'Alex','Diaz','Moreno');
+insert into ExEmpleado values(010,'Panchito','Plata','David');
+insert into ExEmpleado values(011,'Vegeta','SIete','SIete');
+
+
+
+
+
+insert into Nomina values(default,1001,3000);
+insert into Nomina values(default,1002,2500);
+insert into Nomina values(default,1003,2000);
+insert into Nomina values(default,1004,2500);
+insert into Nomina values(default,1005,2500);
+insert into Nomina values(default,1006,2500);
+insert into Nomina values(default,1007,2000);
+insert into Nomina values(default,1008,1800);
+insert into Nomina values(default,1009,2500);
+insert into Nomina values(default,1010,2500);
+insert into Nomina values(default,1011,2500);
+insert into Nomina values(default,1012,2000);
+insert into Nomina values(default,1013,1800);
+insert into Nomina values(default,1014,2500);
+insert into Nomina values(default,1015,1800);
+
+
+
+
+insert into Turno values(1001,1001,'Nocturno');
+insert into Turno values(1002,1002,'Nocturno');
+insert into Turno values(1003,1003,'Vespertino');
+insert into Turno values(1004,1004,'Nocturno');
+insert into Turno values(1005,1005,'Vespertino');
+insert into Turno values(1006,1006,'Nocturno');
+insert into Turno values(1007,1007,'Nocturno');
+insert into Turno values(1008,1008,'Vespertino');
+insert into Turno values(1009,1009,'Nocturno');
+insert into Turno values(1010,1010,'Vespertino');
+insert into Turno values(1011,1011,'Vespertino');
+insert into Turno values(1012,1012,'Nocturno');
+insert into Turno values(1013,1013,'Nocturno');
+insert into Turno values(1014,1014,'Vespertino');
+insert into Turno values(1015,1015,'Nocturno');
+
+insert into Ticket values(1001,1003,468,500,'2018-12-22');
+insert into Ticket values(1002,1003,722,800,'1018-12-29');
+insert into Ticket values(1003,1001,376,400,'2019-12-20');
+insert into Ticket values(1004,1003,280,300,'2018-03-20');
+insert into Ticket values(1005,1002,910,1000,'2019-02-18');
+
+insert into Pedido values (1001,1005,1011,1001,1010);
+insert into Pedido values (1002,1001,1016,1001,1010);
+insert into Pedido values (1003,null,1011,1001,1010);
+insert into Pedido values (1004,null,1006,1001,1010);
+insert into Pedido values (1005,null,1007,1001,1010);
+insert into Pedido values (1006,null,1014,1001,1010);
+insert into Pedido values (1007,null,1007,1001,1010);
+insert into Pedido values (1008,null,1007,1001,1010);
+insert into Pedido values (1009,1006,1016,1002,1015);
+insert into Pedido values (1010,1006,1016,1002,1015);
+insert into Pedido values (1011,1007,1016,1002,1015);
+insert into Pedido values (1012,1007,1016,1002,1015);
+insert into Pedido values (1013,null,1006,1002,1015);
+insert into Pedido values (1014,null,1006,1002,1015);
+insert into Pedido values (1015,null,1006,1002,1015);
+insert into Pedido values (1016,null,1006,1002,1015);
+insert into Pedido values (1017,null,1013,1002,1015);
+insert into Pedido values (1018,null,1005,1002,1015);
+insert into Pedido values (1019,null,1006,1003,1003);
+insert into Pedido values (1020,1008,1007,1003,1005);
+insert into Pedido values (1021,1008,1007,1003,1005);
+insert into Pedido values (1022,null,1007,1003,1005);
+insert into Pedido values (1023,null,1001,1003,1005);
+insert into Pedido values (1024,null,1006,1004,1002);
+insert into Pedido values (1025,null,1006,1004,1002);
+insert into Pedido values (1026,null,1006,1004,1002);
+insert into Pedido values (1027,null,1006,1004,1002);
+insert into Pedido values (1028,null,1006,1004,1002);
+insert into Pedido values (1029,null,1013,1004,1002);
+insert into Pedido values (1030,1001,1016,1005,1008);
+insert into Pedido values (1031,1001,1016,1005,1008);
+insert into Pedido values (1032,1005,1016,1005,1008);
+insert into Pedido values (1033,1011,1016,1005,1008);
+insert into Pedido values (1034,1011,1016,1005,1008);
+insert into Pedido values (1035,null,1016,1005,1008);
+insert into Pedido values (1036,null,1003,1005,1008);
+insert into Pedido values (1037,null,1004,1005,1008);
+insert into Pedido values (1038,null,1003,1005,1008);
+insert into Pedido values (1039,null,1001,1005,1008);
+insert into Pedido values (1040,null,1001,1005,1008);
+insert into Pedido values (1041,null,1013,1005,1008);
+insert into Pedido values (1042,null,1014,1005,1008);
+insert into Pedido values (1043,null,1014,1005,1008);
+
+USE BAR; 
+
+update Empleado set Nombre = 'Juan'
+where ID = 1009;
+
+update Empleado set Nombre = 'Noe'
+where ID = 1004;
+
+update Empleado set Nombre = 'Alonso'
+where ID = 1001;
+
+update Empleado set Telefono = 85910864
+where ID = 1014;
+
+update Nomina set Sueldo = 2500
+where ID_EmpleadoNomina = 1012;
+
+update Nomina set Sueldo = 3000
+where ID_EmpleadoNomina = 1004;
+
+update Nomina set Sueldo = 2500
+where ID_EmpleadoNomina = 1001;
+
+update Bebida set Precio = 30
+where ID = 1015;
+
+update Bebida set Precio = 60
+where ID = 1010;
+
+update Plato set Precio = 50
+where ID = 1012;
+
+delete from ExEmpleado
+where ID = 001;
+
+delete from ExEmpleado
+where ID = 002;
+
+delete from ExEmpleado
+where ID = 003;
+
+delete from ExEmpleado
+where ID = 004;
+
+delete from ExEmpleado
+where ID = 005;
+
+delete from ExEmpleado
+where ID = 006;
+
+delete from ExEmpleado
+where ID = 008;
+
+delete from ExEmpleado
+where ID = 009;
+
+delete from ExEmpleado
+where ID = 010;
+
+delete from ExEmpleado
+where ID = 011;
+
+Select * from Plato;
+Select * from Bebida;
+Select * from Nomina;
+Select * from Sucursal;
+Select * from Pedido;
+Select * from ExEmpleado;
+Select * from Ticket;
+Select * from Empleado;
+Select * from Turno;
+
+
+
